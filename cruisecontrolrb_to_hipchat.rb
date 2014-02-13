2d5c5cdb8823fb0cc428d70282f7711125a596a3
Praveen Selvam <praveen.selvam@gmail.com>
2014-02-13 12:47:46 +0530

    Enabling posting.

--- cruisecontrolrb_to_hipchat.rb
+++ cruisecontrolrb_to_hipchat.rb
@@ -170,7 +170,7 @@ class CruisecontrolrbToHipchat < Sinatra::Base
             color = result == "Passed" ? "green" : "red"
             pipeline["rooms"].each do |room_id|
               puts "Posting to #{room_id} - #{message}"
-              # Hipchat.new.hip_post room_id, message, color
+              Hipchat.new.hip_post room_id, message, color
             end
           end
         else
