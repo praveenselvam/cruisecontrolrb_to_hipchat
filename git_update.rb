e8451ef96cd36f33f60542edd58ec65c81e7985f
Praveen Selvam <praveen.selvam@gmail.com>
2014-02-14 11:03:31 +0530

    Testing one room.

--- git_update.rb
+++ git_update.rb
@@ -14,6 +14,7 @@ ROOMS = {
 } 
 
 GIT_COMMUNICATION_CONFIG = {
+  "hook-bot" => [ROOMS["Git Integ Test"]],
   "business" => [ROOMS["App"], ROOMS["UI"]],
   "indix.com" => [ROOMS["UI"]],
   "apeiron" => [ROOMS["Analytics"]],
