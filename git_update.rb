5104c0b3403b3600c23bdf7385e633d1e756b763
Praveen Selvam <praveen.selvam@gmail.com>
2014-02-14 10:54:44 +0530

    Revert "Fix url"
    This reverts commit f13fc068841a9b4ac2359cb915c20cd5bdc5a3c7.

--- git_update.rb
+++ git_update.rb
@@ -37,7 +37,7 @@ class GitUpdate
       commit_message = commit["message"]
       commit_hash = commit["id"][0..9]
       commit_url = commit["url"]
-      message += "<br/>- #{commit_message} (<a href=\"#{commit_url}\">#{commit_hash}</a>)"
+      message += "<br/>- #{commit_message} (<a href="">#{commit_hash}</a>)"
     end
 
     GIT_COMMUNICATION_CONFIG[repo_name].each do |room_id|
